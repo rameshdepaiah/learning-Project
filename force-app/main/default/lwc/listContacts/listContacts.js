f8664e1de822ad0ef9d391d6f439b7a339092c97
ramesh.depaiah <ramesh.depaiah@hult.edu>
2022-07-09 14:23:46 +0100

    screenflow

---
+++ force-app/main/default/lwc/listContacts/listContacts.js
@@ -0,0 +1,27 @@
+import { LightningElement, wire, api } from 'lwc';
+import getContactList from '@salesforce/apex/LearningController.getContactList';
+import { FlowAttributeChangeEvent } from 'lightning/flowSupport';
+
+export default class ListContacts extends LightningElement {
+    @api outputContactSelected;
+    contactlist;
+
+    columns = [
+        { label: 'First Name', fieldName: 'FirstName' },
+        { label: 'Last Name', fieldName: 'LastName' },
+        { label: 'Email', fieldName: 'Email', type: 'email' },
+        { label: 'Phone', fieldName: 'Phone' }
+    ];
+    @wire(getContactList)
+    wiredContact({ data, error }) {
+        this.contactlist = data;
+    }
+
+    handleRowSelection = event => {
+        var selectedRows = event.detail.selectedRows;
+        this.dispatchEvent(new FlowAttributeChangeEvent('outputContactSelected', selectedRows[0]));
+        console.log(selectedRows[0]);
+    }
+
+
+}
